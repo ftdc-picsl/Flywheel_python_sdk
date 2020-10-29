61befb4a1d441469fb2a19e7414eeeae3168a239
saraheburke <saraheburke@pennmedicine.upenn.edu>
2020-10-29 19:59:20 -0400

    added acquisition field to log file

--- dcm2niix_audit.py
+++ dcm2niix_audit.py
@@ -40,13 +40,12 @@ for i in range(0,len(sesslist)-1):
           sub_log.append(fw.get(acqs[0]["parents"]["subject"]).label)
           sess_log.append(fw.get(acqs[0]["parents"]["session"]).label)
           file_log.append(a.files[0].name)
-          zip_log.append(a.files[0]["zip_member_count"])
           file_id_log.append(files[1]['_id'])
       else:    
           run.append(a.files[0].name)
                       
-d = {'subject':sub_log,'session':sess_log,'file':file_log,'zip_size':zip_log}
-run = {'subject':sub_log, 'session':sess_log,'file':file_log,'zip_size':zip_log}
+d = {'subject':sub_log,'session':sess_log,'file':file_log}
+run = {'subject':sub_log, 'session':sess_log,'file':file_log}
 success_log=pd.DataFrame(d)
 fail_log=pd.DataFrame(run)
 
@@ -63,16 +62,20 @@ ImageType=[]
 sub_list=[]
 sess_log=[]
 flip_angle=[]
+scan_type=[]
+zip_count=[]
 for a in range(0,len(acq_list)-1):
     scan_of_interest=fw.get(acq_list[a])
     sub_list.append(fw.get(scan_of_interest["parents"]["subject"]).label)
     sess_log.append(fw.get(scan_of_interest["parents"]["session"]).label)
+    scan_type.append(scan_of_interest['files'][0]['name'])
     EchoTime.append(scan_of_interest['files'][0]['info']['EchoTime'])
     Modality.append(scan_of_interest['files'][0]['info']['Modality'])
     RepetitionTime.append(scan_of_interest['files'][0]['info']['RepetitionTime'])
     Acquisition_matrix.append(scan_of_interest['files'][0]['info']['Rows'])
     ImageType.append(scan_of_interest['files'][0]['info']['ImageType'][0])
-    flip_angle.append(scan_of_interest['files'][0]['info']['FlipAngle'])  
+    flip_angle.append(scan_of_interest['files'][0]['info']['FlipAngle'])
+    zip_count.append(scan_of_interest['files'][0]['zip_member_count'])
     if not bool(scan_of_interest['files'][0]['info']):
         PixelSpacing.append(["NA"])
     else:
@@ -82,6 +85,6 @@ for a in range(0,len(acq_list)-1):
     else:
         SliceThickness.append(scan_of_interest['files'][0]['info']['SliceThickness'])
         
-e={'subject':sub_list,'session':sess_log,'EchoTime':EchoTime,'Modality':Modality,'RepetitionTime':RepetitionTime,'Acquisition_matrix':Acquisition_matrix,'ImageType':ImageType,'FlipAngle':flip_angle,'SliceThickness':SliceThickness,'PixelSpacing':PixelSpacing}     
+e={'subject':sub_list,'session':sess_log,'scantype':scan_type,'EchoTime':EchoTime,'Modality':Modality,'RepetitionTime':RepetitionTime,'Acquisition_matrix':Acquisition_matrix,'ImageType':ImageType,'FlipAngle':flip_angle,'SliceThickness':SliceThickness,'PixelSpacing':PixelSpacing,'ZipCount':zip_count}     
 parameter_log=pd.DataFrame(e)
 
