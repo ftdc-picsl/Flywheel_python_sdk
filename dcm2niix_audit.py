3412cc74529a9ca6e4621391032b99b0139cfd73
saraheburke <45669546+saraheburke@users.noreply.github.com>
2020-10-31 05:45:23 -0400

    fixed error to failed log output

--- dcm2niix_audit.py
+++ dcm2niix_audit.py
@@ -11,7 +11,7 @@ import pandas as pd
 fw=flywheel.Client()
 
 group = 'pennftdcenter'
-project = 'HCPMultiCenter'
+project = ''
 
 sessions=pd.read_csv("path/to/csv")
 
@@ -28,28 +28,33 @@ zip_log=[]
 file_id_log=[]
 acq_list=[]
 sess_log=[]
+run_sub=[]
+run_sess=[]
 for i in range(0,len(sesslist)-1):
     session = fw.get(sesslist[i])
     acqs=fw.get_session_acquisitions(session.id)
     for j,a in enumerate(acqs):
       files=a.files
       types=[x.type for x in files]
-      if('dicom' in types) and ('nifti' in types):
+      if('dicom' in types) and ('nifti' not in types):
+          run.append(a.files[0].name)
+          run_sub.append(fw.get(acqs[j]["parents"]["subject"]).label)
+          run_sess.append(fw.get(acqs[j]["parents"]["session"]).label)
+      else:  
           acq_list.append(acqs[j].id)
           fw.get(acqs[0]["parents"]["subject"]).label
           sub_log.append(fw.get(acqs[0]["parents"]["subject"]).label)
           sess_log.append(fw.get(acqs[0]["parents"]["session"]).label)
           file_log.append(a.files[0].name)
-          file_id_log.append(files[1]['_id'])
-      else:    
-          run.append(a.files[0].name)
+          file_id_log.append(files[1]['_id']) 
                       
 d = {'subject':sub_log,'session':sess_log,'file':file_log}
-run = {'subject':sub_log, 'session':sess_log,'file':file_log}
+run_log = {'subject':run_sub, 'session':run_sess,'file':run}
 success_log=pd.DataFrame(d)
-fail_log=pd.DataFrame(run)
+fail_log=pd.DataFrame(run_log)
 
 #########extraction of MR parameters, needs troubleshooting for empty dictionary fields.
+###this command will list all keys in acquisition.file
 #ll=pd.DataFrame(scan_of_interest['files'][0]['info'].keys())
 
 PixelSpacing=[]
