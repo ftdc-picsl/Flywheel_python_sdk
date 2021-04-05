6d0a53c293da50dc5a5ed6062e50ab4f887205a4
Philip Cook <cookpa@pennmedicine.upenn.edu>
2021-04-05 14:39:13 -0400

    Also query funding info

--- project_list.py
+++ project_list.py
@@ -4,11 +4,13 @@ fw = flywheel.Client()
 
 group = fw.lookup("pennftdcenter")
 
-print('Project,NumSessions,DateCreated')
+print('Project,NumSessions,DateCreated,AccountNumber')
 
 for proj in group.projects():
 
-    line = {'project': '', 'numSessions': '', 'dateCreated': 'NA'}
+    proj = proj.reload()
+    
+    line = {'project': '', 'numSessions': '', 'dateCreated': 'NA', 'accountNumber': 'None'}
 
     line['project'] = proj.label
 
@@ -16,6 +18,8 @@ for proj in group.projects():
  
     d = proj.created
 
+    line['accountNumber'] = proj.info['ProjectFunding']['accountNumber']
+
     line['dateCreated'] = '{}-{:02}-{:02}'.format(d.year, d.month, d.day)
     
-    print('{project},{numSessions},{dateCreated}'.format(**line))    
+    print('{project},{numSessions},{dateCreated},{accountNumber}'.format(**line))    
